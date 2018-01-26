bae364117f9a9d4e81976e0ce04770d81f575cab
Mika Dumont <mikakrantman@gmail.com>
2018-01-25 19:16:28 -1000

    Got-Space-Ship-To-Move

--- js/game.js
+++ js/game.js
@@ -1,21 +1,49 @@
 (Phaser => {
-  const GAME_WIDTH = 460;
-  const GAME_HEIGHT = 600;
+  console.log(Phaser);
+  const GAME_WIDTH = 400;
+  const GAME_HEIGHT = 400;
   const GAME_CONTAINER_ID = 'game';
+  const INITIAL_MOVESPEED = 4;
+  const GFX = 'gfx';
+  let player;
+  let cursors;
 
-  //
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
+
   function preload() {
+    game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
 
   };
 
   function create() {
-
+    cursors = game.input.keyboard.createCursorKeys();
+    player = game.add.sprite(100, 100, GFX, 8);
+    player.moveSpeed = INITIAL_MOVESPEED;
+  };
+  function update() {
+    //invokes the ship to move
+    handlePlayerMovement();
   };
 
-  function update() {
-
+  //creates movement for the ship
+  function handlePlayerMovement() {
+    switch (true) {
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch (true) {
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
   };
 
 })(window.Phaser);
